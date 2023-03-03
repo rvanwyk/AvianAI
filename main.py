3107487fe9c721c70520dbc20ed06099f0b854ce
Ryan <70180722+rvanwyk@users.noreply.github.com>
2023-03-02 17:34:42 -1000

    Update main.py
    It's alive! Saves PNG files from motion detection.

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import cv2
 import numpy as np
 from picamera2 import Picamera2, MappedArray
 import time
+import os
+
+# Create directory to save images if it doesn't already exist
+if not os.path.exists("motion_images"):
+    os.mkdir("motion_images")
 
 # Grab images as numpy arrays and leave everything else to OpenCV.
 
@@ -15,32 +20,21 @@ picam2.start()
 tmpAvg = 0
 alpha = 0.95
 frame_count = 0
-fps = 30
 frame_size = (640, 480)
-
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = None
 
 while True:
     im = picam2.capture_array()
 
     grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    # tmpAvg = tmpAvg*alpha + (1-alpha)*np.average(im) 
-
     newAvg = np.average(im) 
     print(newAvg)
 
-    if abs(newAvg - tmpAvg) > 10:
+    if abs(newAvg - tmpAvg) > 5:
         print("Motion detected!")
-        if out is not None:
-            out.release()
-        
-        out = cv2.VideoWriter('motion_detected_%d.mp4' % frame_count, fourcc, fps, frame_size)
-    
-    if out is not None:
-        out.write(im)
+        filename = os.path.join("motion_images", f"motion_detected_{frame_count}.jpg")
+        cv2.imwrite(filename, im)
         frame_count += 1
-    
+
     tmpAvg = newAvg
 
     cv2.imshow("Camera", im)
@@ -50,5 +44,4 @@ while True:
 
 picam2.stop()
 cv2.destroyAllWindows()
-
 
