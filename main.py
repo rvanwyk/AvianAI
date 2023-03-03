f1cbdef8baf9aa434b844364ca6d1098281899c9
Ryan <70180722+rvanwyk@users.noreply.github.com>
2023-03-02 19:12:39 -1000

    Update main.py
    This version grabs the images that were collected from the motion detection, and uploads them to a Google Cloud Storage Bucket.

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import cv2
 import numpy as np
 from picamera2 import Picamera2, MappedArray
 import time
+from google.cloud import storage
 import os
 
-# Create directory to save images if it doesn't already exist
-if not os.path.exists("motion_images"):
-    os.mkdir("motion_images")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/maker/open-cv/credentials.json"
+
+# Initialize Google Cloud Storage client
+client = storage.Client()
+
+# Get the bucket object
+bucket_name = 'avian_motion'
+bucket = client.bucket(bucket_name)
 
 # Grab images as numpy arrays and leave everything else to OpenCV.
 
@@ -20,21 +26,31 @@ picam2.start()
 tmpAvg = 0
 alpha = 0.95
 frame_count = 0
+fps = 30
 frame_size = (640, 480)
 
 while True:
     im = picam2.capture_array()
 
     grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # tmpAvg = tmpAvg*alpha + (1-alpha)*np.average(im) 
+
     newAvg = np.average(im) 
     print(newAvg)
 
-    if abs(newAvg - tmpAvg) > 5:
+    if abs(newAvg - tmpAvg) > 10:
         print("Motion detected!")
-        filename = os.path.join("motion_images", f"motion_detected_{frame_count}.jpg")
+        
+        # Save the image to a file
+        filename = 'motion_detected_%d.jpg' % frame_count
         cv2.imwrite(filename, im)
+        
+        # Upload the image file to the bucket
+        blob = bucket.blob('motion_images/%s' % filename)
+        blob.upload_from_filename(filename)
+        
         frame_count += 1
-
+    
     tmpAvg = newAvg
 
     cv2.imshow("Camera", im)
@@ -44,4 +60,3 @@ while True:
 
 picam2.stop()
 cv2.destroyAllWindows()
-
